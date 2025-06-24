7d4a1aa6400fe5d796b383327ccf152617fcac62
Arthur Oliveira <oficial.oliveira.arthur@gmail.com>
2025-06-23 23:47:07 -0400

    previa pronta

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaria: "#132B40", // Azul Escuro - Cor primária da marca
+        secundaria: "#F2CA7E", // Dourado - Cor secundária da marca
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial Nova",
+          "Nimbus Sans",
+          "Arial",
+          "sans-serif",
+        ],
+        serif: ["Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+      },
+    },
   },
   plugins: [],
 };
